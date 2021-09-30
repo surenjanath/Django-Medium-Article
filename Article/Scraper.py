efea6a15e19a4c5e926d0abea57f15459d1cc2e0
Surenjanath Singh <surenjanath.singh@gmail.com>
2021-09-30 10:17:32 -0400

    Added scraper features and added to website

--- Article/Scraper.py
+++ Article/Scraper.py
@@ -1,16 +1,102 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import re
 
-def Scraper():
-    pass
+def Scrape(url,header):
+    Articles = []
+    r = requests.get(url,headers=header)
+    if r.status_code == 200 :
+        soup = bs(r.content, 'lxml')
 
+        sections = soup.find_all('section')
+        Base_Link = url
+        Date_posted = []
+        COMMENTS = []
+        Claps = []
 
+        # ------------------------------
+        # Followers
+        # ------------------------------
+        try:
+          Followers = soup.find('a',string = re.compile(r'Followers')).text
+        except : Followers = 0
 
-url     = 'https://surenjanath.medium.com/'
-header  = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
+        # ------------------------------
+        # Comments
+        # ------------------------------
+        for n in soup.find_all('button'):
+          try:
+            n.div.div.svg
+            try:
+              Comments = int(n.div.div.p.text)
+            except:
+              Comments = 0
+            COMMENTS.append(Comments)
+          except:
+            pass
 
-r = requests.get(url,headers=header)
-if r.status_code == 200 :
-    print(r.content)
-else:
-    print(r.status_code)
+        # ------------------------------
+        # Claps
+        # ------------------------------
+        # for claps in soup.find_all('p'):
+        #   try:
+        #     Claps.append(claps.button.text)
+        #   except: pass
+
+        # ------------------------------
+        # Date Posted
+        # ------------------------------
+        for i in soup.find_all('span'):
+          try:
+            if i.a.p == "None" or i.a.p is None:
+              pass
+            else:
+              Date_posted.append(i.a.p.text)
+          except:
+            pass
+        # ------------------------------
+        # Reading Time
+        # ------------------------------
+        read = soup.find_all('span', string=re.compile(r'min read'))
+        # ------------------------------
+        # Images, Descriptions, Title, Link
+        # ------------------------------
+        k = 0
+
+        # print('[*] Followers : {}'.format(Followers))
+
+        for i in sections :
+          try:
+            try:
+              Image       = i.find('img')['src']
+            except :
+              Image       = None
+            Link        = url + i.find('h1').find_next()['href']
+            Title       = i.h1.text
+            Description = i.text.replace(Title,'')
+
+            # print('[*] Title : {}\n[*] Image : {}\n[*] Description : {}'.format(Title,Image,Description))
+            # print('[*] Reading Time : {}'.format(read[k].text))
+            # print('[*] Date Posted  : {}'.format(Date_posted[k]))
+            # print('[*] Link         : {}'.format(Link))
+            # # print('[*] Claps        : {}'.format(Claps[k]))
+            # print('[*] Comments     : {}'.format( COMMENTS[k]))
+
+            Articles.append({
+                'Title'         : Title,
+                'Image'         : Image,
+                'Reading'       : read[k].text,
+                'Posted'        : Date_posted[k],
+                'Description'   : Description,
+                'Comment'      : COMMENTS[k],
+                'Link'          : Link
+
+            })
+
+            k+=1
+            # print('--------------------------------')
+          except:
+            pass
+    else:
+        print(r.status_code)
+    return Articles
--- Article/views.py
+++ Article/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render, redirect
 from .models import feedback
 from .forms import FeedbackForm
 from django.views.decorators.http import require_POST
-
+from .Scraper import Scrape
 # Create your views here.
 
 def Home(request) :
 
+
+    url     = 'https://surenjanath.medium.com'
+    header  = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
+
+    Articles       = Scrape(url, header)
     FBacks         = feedback.objects
     form           = FeedbackForm()
     context = {
+               'Articles'  : Articles,
                'Feedbacks' : FBacks,
                'Form'      : form,
                }
